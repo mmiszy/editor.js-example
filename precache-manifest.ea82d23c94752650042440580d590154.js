cd8726b531f8af0ddcb7e6aaaea6d04933262cc2
Michal Miszczyszyn <michal@mmiszy.pl>
2019-11-20 17:57:59 +0100

    Updates

---
+++ precache-manifest.ea82d23c94752650042440580d590154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f73b6f41bdab5cea3412c8c692d950",
+    "url": "/editor.js-example/index.html"
+  },
+  {
+    "revision": "b3aa2a03960085c31459",
+    "url": "/editor.js-example/static/css/main.0f0afef3.chunk.css"
+  },
+  {
+    "revision": "b748ed684ea902fe8bba",
+    "url": "/editor.js-example/static/js/2.8e183e65.chunk.js"
+  },
+  {
+    "revision": "b3aa2a03960085c31459",
+    "url": "/editor.js-example/static/js/main.69c1d36c.chunk.js"
+  },
+  {
+    "revision": "ae64910a46088a3f5a90",
+    "url": "/editor.js-example/static/js/runtime-main.15d77d97.js"
+  }
+]);
